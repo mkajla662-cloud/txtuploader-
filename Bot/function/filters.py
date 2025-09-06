67941fffc6e70aa7d5263d84621eb01d68beb1de
mkajla662-cloud <mkajla662@gmail.com>
2025-09-06 13:46:48 +0530

    Update filters.py

--- Bot/function/filters.py
+++
@@ -1,8 +0,0 @@
-from ..config import Config
-from pyrogram import filters
-
-
-if Config.OWNER_ID:
-    OWNER_FILTER = filters.chat(int(Config.OWNER_ID)) & filters.incoming
-else:
-    OWNER_FILTER = filters.incoming
